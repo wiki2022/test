ab7f05a65ad170879da82a4793af876e110b24d3
wiki2022 <100251295+wiki2022@users.noreply.github.com>
2022-02-23 10:25:43 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,25 @@
+def on_a_pressed():
+    mySprite.start_effect(effects.confetti, 500)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+mySprite: Sprite = None
+mySprite = sprites.create(img("""
+        . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(mySprite)
